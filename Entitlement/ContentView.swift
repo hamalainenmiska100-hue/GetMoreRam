519e5b72db93cd41d723531505b6244d3b2836da
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-03-25 14:36:04 +0200

    Update ContentView.swift

--- Entitlement/ContentView.swift
+++ Entitlement/ContentView.swift
@@ -8,25 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var loginViewModel = LoginViewModel.shared
+    @StateObject private var appIDViewModel = AppIDViewModel()
+
     var body: some View {
         TabView {
-            AppIDView(viewModel: AppIDViewModel())
+            AppIDView(viewModel: appIDViewModel)
                 .tabItem {
                     Label("App IDs".loc, systemImage: "square.stack.3d.up.fill")
                 }
-            SettingsView(viewModel: LoginViewModel())
+
+            SettingsView(viewModel: loginViewModel)
                 .tabItem {
                     Label("Settings".loc, systemImage: "gearshape.fill")
                 }
         }
-
         .environmentObject(DataManager.shared.model)
-        
-
-    }
-    
-    func test() {
-        
     }
 }
 
