813f29555368631cffd858e7b1ca7dd2d7df4397
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-03-25 14:34:22 +0200

    Update Keychain.swift

--- Entitlement/Keychain.swift
+++ Entitlement/Keychain.swift
@@ -8,90 +8,103 @@
 
 import Foundation
 import KeychainAccess
-
 import StosSign
 
 @propertyWrapper
 public struct KeychainItem<Value>
 {
     public let key: String
-    
+
     public var wrappedValue: Value? {
         get {
             switch Value.self
             {
-            case is Data.Type: return try? Keychain.shared.keychain.getData(self.key) as? Value
-            case is String.Type: return try? Keychain.shared.keychain.getString(self.key) as? Value
-            default: return nil
+            case is Data.Type:
+                return try? Keychain.shared.keychain.getData(self.key) as? Value
+            case is String.Type:
+                return try? Keychain.shared.keychain.getString(self.key) as? Value
+            default:
+                return nil
             }
         }
         set {
             switch Value.self
             {
-            case is Data.Type: Keychain.shared.keychain[data: self.key] = newValue as? Data
-            case is String.Type: Keychain.shared.keychain[self.key] = newValue as? String
-            default: break
+            case is Data.Type:
+                Keychain.shared.keychain[data: self.key] = newValue as? Data
+            case is String.Type:
+                Keychain.shared.keychain[self.key] = newValue as? String
+            default:
+                break
             }
         }
     }
-    
+
     public init(key: String)
     {
         self.key = key
     }
 }
 
-public class Keychain
+public final class Keychain
 {
     public static let shared = Keychain()
-    
-    fileprivate let keychain = KeychainAccess.Keychain(service: Bundle.main.bundleIdentifier!).accessibility(.afterFirstUnlock).synchronizable(true)
-    
+
+    private static let serviceName = Bundle.main.bundleIdentifier ?? "Entitlement"
+
+    fileprivate let keychain = KeychainAccess.Keychain(service: Keychain.serviceName)
+        .accessibility(.afterFirstUnlock)
+        .synchronizable(true)
+
     @KeychainItem(key: "appleIDEmailAddress")
     public var appleIDEmailAddress: String?
-    
+
     @KeychainItem(key: "appleIDPassword")
     public var appleIDPassword: String?
-    
+
     @KeychainItem(key: "signingCertificatePrivateKey")
     public var signingCertificatePrivateKey: Data?
-    
+
     @KeychainItem(key: "signingCertificateSerialNumber")
     public var signingCertificateSerialNumber: String?
-    
+
     @KeychainItem(key: "signingCertificate")
     public var signingCertificate: Data?
-    
+
     @KeychainItem(key: "signingCertificatePassword")
     public var signingCertificatePassword: String?
-    
+
     @KeychainItem(key: "patreonAccessToken")
     public var patreonAccessToken: String?
-    
+
     @KeychainItem(key: "patreonRefreshToken")
     public var patreonRefreshToken: String?
-    
+
     @KeychainItem(key: "patreonCreatorAccessToken")
     public var patreonCreatorAccessToken: String?
-    
+
     @KeychainItem(key: "patreonAccountID")
     public var patreonAccountID: String?
-    
+
     @KeychainItem(key: "identifier")
     public var identifier: String?
-    
+
     @KeychainItem(key: "adiPb")
     public var adiPb: String?
-    
+
+    @KeychainItem(key: "authStateData")
+    public var authStateData: Data?
+
     private init()
     {
     }
-    
+
     public func reset()
     {
         self.appleIDEmailAddress = nil
         self.appleIDPassword = nil
         self.signingCertificatePrivateKey = nil
         self.signingCertificateSerialNumber = nil
+        self.authStateData = nil
     }
 }
